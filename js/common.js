04fe4839ed2f2024c908db1e8c72a80a69a25851
slabnevitch <handofschpilrain@mail.ru>
2019-11-04 14:36:18 +0500

    scroll mechanism change

--- js/common.js
+++ js/common.js
@@ -1,16 +1,49 @@
 (function() {
-	var videoTop = document.querySelector('.ams-video').getBoundingClientRect().top;
-	console.log(videoTop);
+
+  var videoTop = document.querySelector('.header').getBoundingClientRect().height + document.querySelector('.ams-content__header').getBoundingClientRect().height;
+	// console.log('videoTop= ' + videoTop);
+	// console.log('pageYOffset= ' + pageYOffset );
+	var $elem = $('.ams-left');
+	var elemWidth = $elem.width();
 
 	window.addEventListener('scroll', function() {
+		// console.log(this);
+		var scrolledNum = $(window).scrollTop()+$(window).height(),
+				docHeight = $(document).height();
 
-		if(pageYOffset > videoTop){
+		// if(scrolledNum >= docHeight){
+		// 	console.log('ура! конец страницы!');
+		// 	document.querySelector('.ams-main__content-wrapper').classList.add('aligned-end');
+		// 	document.querySelector('.ams-main').classList.remove('scrolled');
+		// }else{
+		// 	document.querySelector('.ams-main__content-wrapper').classList.remove('aligned-end');
+		// }
+		console.log('pageYOffset= ' + pageYOffset );
+		console.log('videoTop= ' + videoTop);
+		
+		if(pageYOffset > videoTop /*&& scrolledNum < docHeight*/){
 				document.querySelector('.ams-main').classList.add('scrolled');
+				$elem.css('width', elemWidth  + 'px');
 		}else{
+			console.log('else condition is comming');
+			$elem.removeAttr('style');
 				document.querySelector('.ams-main').classList.remove('scrolled');
 
 		}
 
+
+	});
+	
+
+	$(document).ready(function() {
+		$.magnificPopup.open({
+			items: {
+				src: $('#ams-phone-popup')
+			},
+			type: 'inline'
+
+			
+			}, 0);
 	});
 	
 })();
